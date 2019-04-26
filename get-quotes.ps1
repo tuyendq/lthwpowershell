b39ef87666853556812f0b9729046560fa55e07f
Tuyen Dang <tuyendq@gmail.com>
2019-04-26 09:09:39 +0700

    x220: add get-quotes.ps1 file

---
+++ get-quotes.ps1
@@ -0,0 +1,15 @@
+﻿# Get quotes from url and export to csv file
+# region Variables
+$today = Get-Date -Format yyyyMMddHHmm
+$quote_url = "https://www.brainyquote.com/topics/motivational"
+$quote_file = "quotes.txt"
+# endregion Variables
+
+$html = Invoke-WebRequest -Uri $quote_url
+
+$html.RawContentLength # Test
+
+$html.Links | ? {$_.title -eq "view quote" -or $_.title -eq "view author"} 
+
+# Keep innerText
+# Keep title
