7c82ab8c4db1b0fae62ad697bea71e95f9650939
Tuyen Dang <tuyendq@gmail.com>
2019-04-26 09:52:50 +0700

    x220: export to file

--- get-quotes.ps1
+++ get-quotes.ps1
@@ -3,13 +3,19 @@
 $today = Get-Date -Format yyyyMMddHHmm
 $quote_url = "https://www.brainyquote.com/topics/motivational"
 $quote_file = "quotes.txt"
+$quote_file2 = "quotes2.txt"
 # endregion Variables
 
 $html = Invoke-WebRequest -Uri $quote_url
 
 $html.RawContentLength # Test
 
-$html.Links | ? {$_.title -eq "view quote" -or $_.title -eq "view author"} 
-
 # Keep innerText
 # Keep title
+$html.Links | ? {$_.title -eq "view quote" -or $_.title -eq "view author"} | Select-Object innerText, title | Export-Csv -Path $quote_file -NoTypeInformation
+
+# Combine "quote","author"
+
+(Get-Content -Raw $quote_file) -replace '"view quote"\r\n','' -replace ',"view author"','' | Out-File $quote_file2
+# (Get-Content -Raw $quote_file) -replace ',"view author"',''
+
