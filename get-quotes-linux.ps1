36488fcbfc0b0f5faf6b75ec885c01024b2a85ac
Tuyen Dang <tuyendq@gmail.com>
2019-04-27 15:54:35 +0700

    000: add get-quotes-linux.ps1

---
+++ get-quotes-linux.ps1
@@ -0,0 +1,17 @@
+﻿# Get quotes from url and export to csv file
+# region Global variables
+$today = Get-Date -Format yyyyMMdd
+$quote_url = "https://www.brainyquote.com/topics/motivational"
+$raw_file = "quotes-raw.txt"
+$quote_file = "quotes" + $today + ".csv"
+# endregion Global variables
+
+# Get site's raw html content
+$html = Invoke-WebRequest -Uri $quote_url
+
+# Keep innerText, title
+$html.Links | ? {$_.title -eq "view quote" -or $_.title -eq "view author"} | Select-Object innerText, title | Export-Csv -Path $raw_file -NoTypeInformation
+
+# Combine "quote","author" into csv format
+(Get-Content -Raw $raw_file) -replace '"innerText","title"','' -replace '^\r\n','' -replace '"view quote"\r\n','' -replace ',"view author"','' | Out-File $quote_file
+# End of script
