963135275be79552591ea7588cafd9fb3ed9c50b
Tuyen Dang <tuyendq@gmail.com>
2021-12-23 20:26:37 +0700

    Add comment

--- string2urlfriendly.ps1
+++ string2urlfriendly.ps1
@@ -3,6 +3,7 @@
 if ($args.Count -ne 1) {
     Write-Host `nUsage: $MyInvocation.MyCommand `"String to be converted`"`n
 } else {
+    # replace spaces or ':' with '-', then replace (one or more '-') with '-'
     $Result = $args[0].tolower() -replace '\s+|:','-' -replace '-+','-'
     Write-Host $Result
 }
