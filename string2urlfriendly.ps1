0f15bb952627b0b88b8aa4e9d23a2eebf82041b1
Tuyen Dang <tuyendq@gmail.com>
2021-12-23 20:22:38 +0700

    string2urlfriendly.ps1: improve

--- string2urlfriendly.ps1
+++ string2urlfriendly.ps1
@@ -3,6 +3,6 @@
 if ($args.Count -ne 1) {
     Write-Host `nUsage: $MyInvocation.MyCommand `"String to be converted`"`n
 } else {
-    $Result = $args[0].replace(" ","-").tolower()
+    $Result = $args[0].tolower() -replace '\s+|:','-' -replace '-+','-'
     Write-Host $Result
 }
