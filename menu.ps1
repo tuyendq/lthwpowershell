216814e633b02a68683a2aaad818527011753fa0
Tuyen Dang <tuyendq@gmail.com>
2022-10-26 23:15:43 +0700

    update menu.ps1

--- menu.ps1
+++ menu.ps1
@@ -6,6 +6,20 @@ function Show-Menu {
     # )
     
     Write-Host "=========== What do you want ==========="
+    Write-Host "Press '1': Go to LinkedIn Learning"
+    Write-Host "Press '2': Go to .\projects\github'"
+
+    $selection = Read-Host "Press a number"
+
+    switch ($selection) {
+        1 { 
+            Write-Host "You press 1 => linkedin-learning"
+            Set-Location "D:\projects\linkedin-learning\"
+         }
+        Default {}
+    }
+
+    Pause
 }
 
 Show-Menu
