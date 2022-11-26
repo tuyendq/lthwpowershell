0d75259d5d4dd8566fede098b1bc178f0597d94b
Tuyen Dang <tuyendq@gmail.com>
2022-11-26 16:15:34 +0700

    menu.ps1: add optional parameter to Show-Menu function

--- menu.ps1
+++ menu.ps1
@@ -1,25 +1,41 @@
-﻿# menu.ps1
-
 function Show-Menu {
-    # param (
-    #     OptionalParameters
-    # )
-    
-    Write-Host "=========== What do you want ==========="
-    Write-Host "Press '1': Go to LinkedIn Learning"
-    Write-Host "Press '2': Go to .\projects\github'"
+    param (
+      [Int] $optionNumber
+    )
+    Write-Host "========== What do you want ============"
+    Write-Host "'1' - Go to LinkedIn Learning"
+    Write-Host "'2' - Go to Projects"
+    Write-Host "'3' - Go to LinkedIn Learning"
+	Write-Host "'9' - Go to lthwflutter"
+    Write-Host "'q' - Quit"
 
-    $selection = Read-Host "Press a number"
+    if ($optionNumber) {
+        $selection = $optionNumber    
+    } else {
+        $selection = Read-Host "Select an option"
+    }
 
     switch ($selection) {
         1 { 
-            Write-Host "You press 1 => linkedin-learning"
-            Set-Location "D:\projects\linkedin-learning\"
-         }
+            Set-Location "D:\projects\linkedin-learning\" 
+        }
+        2 { 
+            Set-Location "D:\projects\" 
+        }
+        3 { 
+            Set-Location "D:\projects\" 
+        }
+        9 { 
+            Set-Location "D:\projects\lthwflutter\" 
+        }
         Default {}
     }
+}
 
-    Pause
+if ($args.Count -eq 1){
+    Write-Host "You give one argument" $args[0]
+    Show-Menu($args[0])
+    Exit(0)
 }
 
 Show-Menu
